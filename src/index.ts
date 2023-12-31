dca9accc3abe7aae842f269689589a4e1c55cd86
kamilkodzi <bizkamiladamski@gmail.com>
2023-12-31 14:59:50 +0100

    splitting chunk logic in to pieces

--- src/index.ts
+++ src/index.ts
@@ -13,20 +13,62 @@ class ReplaceStream extends Transform {
   }
 
   _transform(
-    chunk: any,
+    chunk: Buffer,
     encoding: BufferEncoding,
     callback: TransformCallback
   ): void {
-    const pieces = (this.tail + chunk).split(this.searchStr)
-    const lastPiece = pieces[pieces.length - 1]
-    const tailLen = this.searchStr.length - 1
-    this.tail = lastPiece.slice(-tailLen)
-    pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen)
-    this.push(pieces.join(this.replaceStr))
-    callback()
+    const lastOfferClosingTag = (this.tail + chunk.toString()).lastIndexOf(
+      '</offer>'
+    )
+
+    if (lastOfferClosingTag !== -1) {
+      // To be handled
+      console.log('last index of </offer>: ', lastOfferClosingTag)
+      const pieces = (this.tail + chunk.toString()).split('</offer>')
+      console.log('length of pieces: ', pieces.length)
+      pieces.pop()
+      // console.log(pieces)
+      // It
+      // this.tail = chunk.subarray(lastOfferClosingTag + 8).toString()
+      // if (pieces.length > 1) {
+      console.log('There is more elements in pieces')
+      // there is just one element in array
+      const joined = pieces.join('</offer_new>') + '</offer_new>'
+      this.tail = this.tail + chunk.toString()
+      console.log(joined)
+      // } else {
+      //   console.log('There is just one element in pieces')
+      //   // there is just one element in array
+      //   const joined = pieces[0] + '</offer_new>'
+      //   console.log(joined)
+      // }
+    } else {
+      // there is no </offer> tag in previous + current chunk
+      // so all is stored in to tail propertie for further processing
+      this.tail = this.tail + chunk.toString()
+      console.log('There is no </offer> in current buffer')
+      // Check how to filter data - if there is curicumstances that we dont want send data,
+      // just to iterate to next cb - to be checked hot to do not send / push data
+    }
+
+    // this.tail = chunk.subarray(lastOfferClosingTag + 8)
+
+    // console.log(pieces)
+    this.tail = chunk.subarray(lastOfferClosingTag + 8).toString()
+    // console.log('pieces, ', pieces)
+    // console.log(this.tail.toString())
+    // console.log(chunk.toString())
+    // const pieces = (this.tail + chunk).split(this.searchStr)
+    // const lastPiece = pieces[pieces.length - 1]
+    // const tailLen = this.searchStr.length - 1
+    // this.tail = lastPiece.slice(-tailLen)
+    // pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen)
+
+    // this.push(pieces.join(this.replaceStr))
+    // callback()
   }
   _flush(callback: TransformCallback): void {
-    this.push(this.tail)
+    // this.push(this.tail)
     callback()
   }
 }
