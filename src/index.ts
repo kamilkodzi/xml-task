705de4714c705e1175ea1312efece5ef040e68e5
kamilkodzi <bizkamiladamski@gmail.com>
2023-12-31 13:40:25 +0100

    Changing EUR to PLN via stream
    (cherry picked from commit e853e2cee5e29df72a02a3260a280967c46b881e)

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,45 @@
 import fs from 'node:fs'
 import { Transform } from 'node:stream'
+import { TransformCallback } from 'stream'
+import path from 'path'
+
+class ReplaceStream extends Transform {
+  tail: string
+  searchStr = 'EUR'
+  replaceStr = 'PLN'
+  constructor(opts?: any) {
+    super({ ...opts })
+    this.tail = ''
+  }
+
+  _transform(
+    chunk: any,
+    encoding: BufferEncoding,
+    callback: TransformCallback
+  ): void {
+    const pieces = (this.tail + chunk).split(this.searchStr)
+    const lastPiece = pieces[pieces.length - 1]
+    const tailLen = this.searchStr.length - 1
+    this.tail = lastPiece.slice(-tailLen)
+    pieces[pieces.length - 1] = lastPiece.slice(0, -tailLen)
+    this.push(pieces.join(this.replaceStr))
+    callback()
+  }
+  _flush(callback: TransformCallback): void {
+    this.push(this.tail)
+    callback()
+  }
+}
 
 const main = async () => {
-  const readStream = fs.createReadStream('src/data/feed_sample.xml')
-  const writeStream = fs.createWriteStream('src/data/feed_sample_out.xml')
+  const feedURL = path.join(__dirname, './data/feed_sample.xml')
+  const feedOutUrl = path.join(__dirname, './data/feed_sample_out.xml')
+  const readStream = fs.createReadStream(feedURL, {
+    highWaterMark: 3 * 1024,
+  })
+  const writeStream = fs.createWriteStream(feedOutUrl)
 
-  readStream
-    .pipe(
-      new Transform({
-        // to be moved out and to create state for chunks-ends
-        writableObjectMode: true,
-        transform(chunk, encoding, callback) {
-          console.log('data chunk: ', chunk)
-          callback(null, chunk)
-        },
-      })
-    )
-    .pipe(writeStream)
+  readStream.pipe(new ReplaceStream()).pipe(writeStream)
 }
 
 main()
